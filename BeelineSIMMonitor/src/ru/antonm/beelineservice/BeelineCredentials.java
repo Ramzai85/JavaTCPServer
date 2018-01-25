884c2db86b65583eee723483fdbdd6467bba53be
Антон <@192.168.1.112>
2018-01-26 01:10:12 +0300

    First commit

---
+++ BeelineSIMMonitor/src/ru/antonm/beelineservice/BeelineCredentials.java
@@ -0,0 +1,7 @@
+package ru.antonm.beelineservice;
+
+public class BeelineCredentials {
+	public static final String weburl = "https://restapi10.jasper.com/rws/api/v1/devices/";
+	public static final String login = "AMAKAROV24";
+	public static final String password = "37220dfe-3095-446e-ac7f-f409caed3264";
+}
---
+++ BeelineSIMMonitor/src/ru/antonm/beelineservice/SomeClass.java
@@ -0,0 +1,10 @@
+package ru.antonm.beelineservice;
+
+import org.json.JSONException;
+
+public class SomeClass {
+	public static void main(String[] args) throws JSONException{
+		BeelineConnector connector = new BeelineConnector("8970199170384027759");
+		connector.getLastIMEI();
+	}
+}
